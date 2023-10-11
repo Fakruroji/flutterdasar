d897d76156fd1ed31d4c2c1678875ad9a50fb112
Rosid Mustofa <75148389+rosid-codelab@users.noreply.github.com>
2023-10-11 14:21:14 +0700

    Update text_widget.dart

--- lib/basic_widget/text_widget.dart
+++ lib/basic_widget/text_widget.dart
@@ -7,7 +7,7 @@ class TextWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("FIC - Text"),
+        title: const Text("MobDev - Text"),
         actions: const [],
       ),
       body: Container(
